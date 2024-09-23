10d2702a837a3ad9ef0ad074b07602850fa3a2f5
gwynnn297 <phuchoang29072000@gmail.com>
2024-09-23 09:23:40 +0700

    update hover cửa hàng mới

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@
     } else {
         console.error("Element '#menu-bars' or '.navbar' not found");
     }
+
     var swiperContainer = document.querySelector('.house-slider');
     if (swiperContainer) {
         var swiper = new Swiper(".house-slider", {
@@ -31,4 +32,21 @@
     } else {
         console.error("Element '.house-slider' not found");
     }
+
+
+    window.onscroll = () =>{
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('active');
+    }
+    document.querySelector('#search-icon').onclick = () =>{
+        document.querySelector('#search-form').classList.toggle('active');
+
+    }
+    document.querySelector('#close').onclick = () =>{
+        document.querySelector('#search-form').classList.remove('active');
+
+    }
+    
+
+
 
