629d3b879252632e329b95bbffab0a6a96105fd4
gwynnn297 <phuchoang29072000@gmail.com>
2024-11-04 11:06:41 +0700

    đăng nhập đăng ký local storage

---
+++ register_login/js/main.js
@@ -0,0 +1,34 @@
+function register(e){
+    event.preventDefault();
+    var username = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("pass").value;
+    var user = {
+        username : username,
+        email :email,
+        password : password
+    }
+    var json = JSON.stringify(user);
+    localStorage.setItem(username,json);
+    alert("đăng ký thành công")
+}
+function login(e){
+    event.preventDefault();
+    var username = document.getElementById("your_name").value;
+    var password = document.getElementById("your_pass").value;
+    var user = localStorage.getItem(username);
+    var data = JSON.parse(user);
+    if(user == null){
+        alert("vui lòng nhập tên tài khoản và mật khẩu");
+    }
+    else if(username == data.username && password == data.password){
+        alert("đăng nhập thành công")
+        localStorage.setItem("loggedInUser", username);
+        window.location.href="../home.html"
+    }
+    else{
+        alert("đăng nhập thất bại");
+    }
+
+
+}
